7c5a7535276dd9382b4e5a64195bc6f158a5caaf
pashpashpash <nik@nugbase.com>
2022-12-28 17:21:08 -0800

    updated truffle.js with polygon network, contract dir config, and  key env variables

--- truffle.js
+++ truffle.js
@@ -1,19 +1,28 @@
 var NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker");
-var HDWalletProvider = require("truffle-hdwallet-provider");
+var HDWalletProvider = require("@truffle/hdwallet-provider");
 
-var mnemonic = process.env.CONTRACTS_MNEMONIC_SEED
-var node_url = process.env.ETH_NODE_URL
+var mnemonic = process.env.CONTRACTS_MNEMONIC_SEED;
+var node_url = process.env.ETH_NODE_URL;
+var infura_key = process.env.INFURA_KEY;
+var alchemy_key = process.env.ALCHEMY_KEY;
 
 if (!mnemonic || !node_url) {
   console.log('WARN: mnemonic or eth_node env variables not set, see README.md');
 }
 
 module.exports = {
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    }
+  contracts_directory: './contracts',
+  contracts_build_directory: './build/contracts',
+  compilers: {
+      solc: {
+          version: '^0.8.3',
+          settings: {
+              optimizer: {
+                  enabled: true,
+                  runs: 200,
+              },
+          },
+      },
   },
   networks: {
     ganache: {
@@ -44,6 +53,30 @@ module.exports = {
       network_id: "1",
       gas: 4000000, // 21,000 normal; 1,685,639 used by v1 Flower Contract
       gasPrice: 20000000000 // really helps to have a 4x safelow fee
+    },
+    polygon: {
+      provider: () => {
+          return new HDWalletProvider({
+              mnemonic: {
+                  phrase: mnemonic,
+              },
+              providerOrUrl:
+                  'https://polygon-mainnet.infura.io/v3/' + infura_key,
+              // providerOrUrl:
+              //     'wss://polygon-mainnet.g.alchemy.com/v2/' + alchemy_key,
+              numberOfAddresses: 1,
+              shareNonce: true,
+          });
+      },
+      network_id: 137,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      chainId: 137,
+      gas: 6600000,
+      gasPrice: 600000000000,
+      networkCheckTimeout: 1000000,
+      timeoutBlocks: 200,
     }
   }
 };
